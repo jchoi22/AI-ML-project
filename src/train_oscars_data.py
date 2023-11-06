2c08dbd8adbcc946a03daef3046410d07f562f54
Jaylen Choi <jaylenchoi@dhcp-10-7-193-64.dhcp.nd.edu>
2023-11-06 15:10:30 -0500

    yuh

---
+++ src/train_oscars_data.py
@@ -0,0 +1,37 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Load your dataset (replace 'your_dataset.csv' with your actual dataset file)
+data = pd.read_csv('your_dataset.csv')
+
+# Data preprocessing (feature selection, encoding, etc.)
+# Make sure to preprocess your data as needed
+
+# Split the data into features (X) and target (y)
+X = data.drop('Oscar_Winner', axis=1)  # Adjust the column name as per your data
+y = data['Oscar_Winner']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the Random Forest classifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = clf.predict(X_test)
+
+# Model evaluation
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+# Print the evaluation metrics
+print(f'Accuracy: {accuracy:.2f}')
+print(f'Precision: {precision:.2f}')
+print(f'Recall: {recall:.2f}')
+print(f'F1 Score: {f1:.2f}')
